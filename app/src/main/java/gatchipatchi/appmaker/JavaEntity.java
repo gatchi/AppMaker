984c6d678af0775b218ced6223a5cf149b6258bb
gatchi <christen.got@gmail.com>
2017-03-19 22:39:31 -0700

    git init

---
+++ app/src/main/java/gatchipatchi/appmaker/JavaEntity.java
@@ -0,0 +1,9 @@
+package gatchipatchi.appmaker;
+
+public abstract class JavaEntity
+{
+	final static int PROTECTED = 0;
+	final static int PUBLIC = 1;
+	final static int PRIVATE = 2;
+	final static int METHOD_TYPE = 3;
+}
---
+++ app/src/main/java/gatchipatchi/appmaker/JavaMethod.java
@@ -0,0 +1,13 @@
+package gatchipatchi.appmaker;
+
+public class JavaMethod
+{
+	static final String TYPE_NAME = "method";
+	
+	int visibility;
+	
+	JavaMethod(int visibility)
+	{
+		this.visibility = visibility;
+	}
+}
