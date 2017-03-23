40d6eb843fab8b4c0e484f5438bf68b311427727
gatchi <christen.got@gmail.com>
2017-03-22 19:10:03 -0700

    module is now an interface
    also added method layout

--- src/main/java/gatchipatchi/appmaker/Module.java
+++ src/main/java/gatchipatchi/appmaker/Module.java
@@ -3,14 +3,7 @@ package gatchipatchi.appmaker;
 import java.util.*;
 import android.view.*;
 
-public class Module
+public interface Module
 {
-	ViewGroup gui;  // or maybe just a layout (xml)
-	List<String> canContain;
-	
-	public Module(ViewGroup gui, List<String> canContain)
-	{
-		this.gui = gui;
-		this.canContain = canContain;
-	}
+	String getModuleType();
 }
--- src/main/java/gatchipatchi/appmaker/modules/Method.java
+++ src/main/java/gatchipatchi/appmaker/modules/Method.java
@@ -1,12 +1,30 @@
 package gatchipatchi.appmaker.modules;
+import android.app.*;
+import android.util.*;
 import android.view.*;
 import gatchipatchi.appmaker.*;
+import org.xmlpull.v1.*;
+import android.widget.*;
 import java.util.*;
 
-public class Method extends Module
+public class Method
 {
-	Method(ViewGroup gui, List<String> canContain)
+	ViewGroup gui;
+	List<String> canContain;
+	String moduleType = "method";
+	
+	Method(Application context)
 	{
-		super(gui, canContain);
+		XmlPullParser parser = context.getResources().getXml(R.layout.method);
+		AttributeSet attributes = Xml.asAttributeSet(parser);
+		gui = new RelativeLayout(context, attributes);
+		
+		canContain = new ArrayList<String>();
+		canContain.add("class");
+	}
+	
+	String getModuleType()
+	{
+		return moduleType;
 	}
 }
